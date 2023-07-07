16157a652f50c3ced4424de5aa05480aec4b5555
phamj0 <107446014+phamj0@users.noreply.github.com>
2023-07-06 23:09:46 -0400

    Co-authored-by: Nicolas Uribe <UribeNi20@users.noreply.github.com>

---
+++ CAP4630/main.py
@@ -0,0 +1,31 @@
+
+class AntColonyOpt:
+    def __init__(self, attractions, ant_count = 100 ):
+        self.ant_count = ant_count                      # number of ants that will be used in algorithm, default is at 100 if no value is entered
+        self.attractions = attractions                  # the array of attractions
+        self.attraction_count = len(attractions)        # number of attractions 
+
+    def ant(self):
+        visited_attraction = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CodePath/Practice/LinkedList.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+'''
+Understand
+Input: head of linked list
+Output: head of edited linked list
+1. Removing nth node from the end of list
+2. Does not have to be sorted
+3. Singly Linked list
+
+Match
+Temporary head 
+
+
+'''
+
+
+
+
+print(6//2)
