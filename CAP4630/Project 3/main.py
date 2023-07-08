ecdfbfd43184d16918aafa88b1e0eea48a229999
phamj0 <107446014+phamj0@users.noreply.github.com>
2023-07-07 23:27:14 -0400

    modified project three

--- CAP4630/Project 3/main.py
+++ CAP4630/Project 3/main.py
@@ -1,9 +1,10 @@
 
 class AntColonyOpt:
-    def __init__(self, attractions, ant_count = 100 ):
+    def __init__(self, attractions, pheromone_rate, ant_count = 100):
         self.ant_count = ant_count                      # number of ants that will be used in algorithm, default is at 100 if no value is entered
         self.attractions = attractions                  # the array of attractions
         self.attraction_count = len(attractions)        # number of attractions 
+        self.pheromone_rate = pheromone_rate
 
     def ant(self):
         visited_attraction = []
